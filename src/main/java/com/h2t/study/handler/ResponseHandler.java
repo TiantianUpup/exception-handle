44b4ce43c6b10b591100e730ff6b654c861a6b8b
unknown <hetiantian@spc-hz20121817.hikvision.com>
2019-08-02 14:23:28 +0800

    âadd log

--- src/main/java/com/h2t/study/handler/ResponseHandler.java
+++ src/main/java/com/h2t/study/handler/ResponseHandler.java
@@ -2,10 +2,14 @@ package com.h2t.study.handler;
 
 import com.h2t.study.exception.CustomException;
 import com.h2t.study.response.BaseResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 统一返回结果异常处理类
@@ -17,13 +21,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 @ResponseBody
 public class ResponseHandler {
+    private static Logger logger = LoggerFactory.getLogger(ResponseHandler.class);
+
     @ExceptionHandler({CustomException.class})
-    public BaseResponse customException(CustomException e) {
+    public BaseResponse customException(CustomException e, HttpServletRequest request) {
+        logger.error(request.getRequestURI() + ":服务运行异常",e);
         return BaseResponse.fail(e.getErrorCodeEnum().getErrorCode(), e.getErrorCodeEnum().getErrorMsg());
     }
 
     @ExceptionHandler({Exception.class})
-    public BaseResponse otherResponse(Exception e) {
+    public BaseResponse otherResponse(Exception e, HttpServletRequest request) {
+        logger.error(request.getRequestURI() + ":自定义异常",e);
         return BaseResponse.fail(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()),
                 e.getMessage());
     }
